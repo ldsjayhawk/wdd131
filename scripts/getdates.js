72a492264acef122c0dd1722342717a680676506
ldsjayhawk <crisbecca@gmail.com>
2024-09-29 00:52:54 -0500

    updated working javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,6 @@
 const d = new Date();
 let currentyear = d.getFullYear();
 document.getElementById("currentyear").innerHTML = currentyear;
+
+const date = new Date(document.lastModified);
+document.getElementById("lastmodified").innerHTML = date;
