9755650d9784f46a37c676ff65078fc9cc3ec7d0
ldsjayhawk <crisbecca@gmail.com>
2024-10-17 19:44:49 -0500

    fixed windchill formula

--- scripts/place.js
+++ scripts/place.js
@@ -1,13 +1,19 @@
-// weatheer
-const temperature = 23;
+// weather
+const temp = 23;
 const condition = "Clear";
 const wind = 11;
-let windChill = 35.74 + 0.6215 * temp - 35.75 * wind ** 0.16 + 0.4275 * temp * wind ** 0.16;
 
-document.getElementById("temp").innerHTML = temperature;
+// calculate wind chill
+function windChill(temp, wind) {
+     return 35.74 + (0.6215 * temp) - (35.75 * (wind ** 0.16)) + (0.4275 * temp * (wind ** 0.16))
+};
+
+let wc = windChill(temp, wind);
+
+document.getElementById("temp").innerHTML = temp;
 document.getElementById("condition").innerHTML = condition;
 document.getElementById("wind").innerHTML = wind;
-document.getElementById("windChill").innerHTML = windChill;
+document.getElementById("windChill").innerHTML = Math.round(wc);
 
 
 // dates in footer
