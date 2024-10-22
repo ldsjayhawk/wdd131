4121d1c3f57372a59cdf0aec5fcc850cfb2f44e7
ldsjayhawk <crisbecca@gmail.com>
2024-10-22 01:01:15 -0500

    form assignment

---
+++ scripts/form.js
@@ -0,0 +1,36 @@
+// populate select dropdown
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+// dates in footer
+const d = new Date();
+let currentyear = d.getFullYear();
+document.getElementById("currentyear").innerHTML = currentyear;
+
+const date = new Date(document.lastModified);
+document.getElementById("lastmodified").innerHTML = date;
